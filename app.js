3807e5ce33ec08268b370fd4660b409e4f85232a
Joanna <askat58@gmail.com>
2021-02-05 19:11:15 +0000

    add axios and scripts

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-let apiKey = "6e2c2fe6da4f8d0c51f51185fb4d24bf";
+function displayData(response) {
+
+}
+
+
+let apiKey = `6e2c2fe6da4f8d0c51f51185fb4d24bf`;
+let apiUrl = `http://api.marketstack.com/v1/currencies`;
+
+axios.get(apiUrl).then(displayData);
