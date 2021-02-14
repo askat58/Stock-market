054cf7a157fc0964b211aa720a731b4e0e5a66d5
Joanna <askat58@gmail.com>
2021-02-14 23:02:54 +0000

    build javascript
    add console log test javascript
    add some border styles

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 function displayData(response) {
+  console.log(response.data.data[0].code)
 
 }
 
 
 let apiKey = `6e2c2fe6da4f8d0c51f51185fb4d24bf`;
-let apiUrl = `http://api.marketstack.com/v1/currencies`;
+let apiUrl = `http://api.marketstack.com/v1/currencies?access_key=${apiKey}`;
 
 axios.get(apiUrl).then(displayData);
